506e3085fad65b62ddd55851f3f9ca000ac7126a
TRClassic <iconcalzone@gmail.com>
2022-03-20 17:27:02 -0400

    Update peds.lua

--- client/peds.lua
+++ client/peds.lua
@@ -8,9 +8,9 @@ CreateThread(function()
     while ( not HasModelLoaded( GetHashKey( "s_m_y_construct_01" ) ) ) do
         Wait(1)
     end
-    lumberjack1 = CreatePed(1, 0x94562DD7, ClassicMan, false, true)
-    lumberjack2 = CreatePed(1, 0x94562DD7, LumberTR, false, true)
-    lumberjack3 = CreatePed(1, 0x94562DD7, sellClassic, false, true)
+    lumberjack1 = CreatePed(1, 0xD7DA9E99, ClassicMan, false, true)
+    lumberjack2 = CreatePed(1, 0xD7DA9E99, LumberTR, false, true)
+    lumberjack3 = CreatePed(1, 0xD7DA9E99, sellClassic, false, true)
     SetEntityInvincible(lumberjack1, true)
     SetBlockingOfNonTemporaryEvents(lumberjack1, true)
     FreezeEntityPosition(lumberjack1, true)
