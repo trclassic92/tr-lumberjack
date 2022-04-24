56da666536e844778a2c5b1d83d9909070eed966
Benzo <102178921+Benzo00@users.noreply.github.com>
2022-04-24 15:53:28 +0200

    Update items.sql

--- items.sql
+++ items.sql
@@ -1,5 +1,13 @@
 
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-	('tree_lumber', 'tree_lumber', 1, 0, 1),
+   ('tree_lumber', 'tree_lumber', 1, 0, 1),
   ('tree_bark', 'tree_bark', 1, 0, 1),
   ('wood_plank ', 'wood_plank ', 1, 0, 1);
+
+
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+  (1000, 'lumberjack', 0, 'lumberjack', 'Worker', 0, '', '');
+
+
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+      ('lumberjack', 'Lumberjack', 1);
